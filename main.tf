b6ddfdf23dfff944f8c3938c9d3cbbd4e15dc8da
Aamir Patel <paamir89@gmail.com>
2024-11-22 09:33:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,18 @@
-test
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_vpc" "dev-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "dev-vpc"
+  }
+}
